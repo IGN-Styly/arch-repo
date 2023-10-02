f3d88c2128f264f2848353e8e5e101036efc1f17
IGN-Styly <claudioresendeptbr@gmail.com>
2023-10-02 15:32:54 +0100

    Update

---
+++ clean.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/bash
+rm ./x86_64/arch-repo.files
+rm ./x86_64/arch-repo.db
+mv ./x86_64/arch-repo.db.tar.gz ./x86_64/arch-repo.db 
+mv ./x86_64/arch-repo.files.tar.gz ./x86_64/arch-repo.files
